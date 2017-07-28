62f5978762d91975c10d8a63471b4726f0099836
Matt Nichols <nich331@gmail.com>
2017-07-28 19:13:41 -0400

    assignment 3 angular completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@
       })
          .state('album', {
              url: '/album',
-             templateUrl: '/templates/album.html'
+             controller: 'AlbumCtrl as album',
+             templateUrl: 'templates/album.html'
       })
          .state('collection', {
              url: '/collection',
